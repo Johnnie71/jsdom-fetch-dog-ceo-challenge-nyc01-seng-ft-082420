5a1fab41b3880648cfff6702f4d6f0b3bc0b80b2
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-09 23:10:30 -0400

    2 deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentloaded', function(){
+
+ 
+fetchPictures();
+fetchBreeds();
+
+});
+
+
+
+function fetchPictures() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+       fetch(imgUrl)
+        .then(res => res.json())
+        .then(res => {
+            res.message.forEach(image => addDogImage(image))
+        });
+}
+
+function addDogImage(picUrl) {
+    const dogContainer = document.querySelector('#dog-image-container');
+    let newImage = document.createElement("img");
+    newImage.src = picUrl;
+    dogContainer.appendChild(newImage);
+}
+
+function fetchBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(list => {
+        list.message.forEach(breed => addBreed(breed));
+    });
+}
+
+function addBreed(breedName){
+    const dogBreeds = document.querySelector('#dog-breeds');
+    let newBreedLi = document.createElement("li");
+    newBreedLi.innerText = breedName;
+    dogBreeds.appendChild(newBreedLi);
+}
+
+addDogImage();
+addBreed();
